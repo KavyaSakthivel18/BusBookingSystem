9b2327b792d034feee3d0d287ba52567cffa1c3a
Kavya <2k22cse077@kiot.ac.in>
2026-04-25 11:21:07 +0530

    initialized backend files

---
+++ Backend/BusbookingSys/src/main/java/com/example/BusbookingSys/BusbookingSysApplication.java
@@ -0,0 +1,13 @@
+package com.example.BusbookingSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusbookingSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusbookingSysApplication.class, args);
+	}
+
+}
---
+++ Backend/BusbookingSys/src/test/java/com/example/BusbookingSys/BusbookingSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BusbookingSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusbookingSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
